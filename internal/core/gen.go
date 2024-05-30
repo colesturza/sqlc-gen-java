f9768e0889e6ed89a3110572fc30ced6b01042da
Cole Sturza <colesturza@gmail.com>
2024-05-30 05:14:48 -0600

    add database type conversions and interface and models templates

--- internal/core/gen.go
+++ internal/core/gen.go
@@ -1,1 +1,19 @@
 package core
+
+import (
+	"strings"
+
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
+
+	"github.com/sqlc-dev/plugin-sdk-go/plugin"
+)
+
+func dataClassName(name string, settings *plugin.Settings) string {
+	out := ""
+	caser := cases.Title(language.Und, cases.NoLower)
+	for _, p := range strings.Split(name, "_") {
+		out += caser.String(p)
+	}
+	return out
+}
